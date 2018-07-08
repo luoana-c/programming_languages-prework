2990025cc98ceaf08272effaaa326c62f78ce1a9
Luoana Chirita <luoana.livadariu@gmail.com>
2018-07-08 09:56:00 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,18 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style, language|
+    
+    language.each do |lang, data|
+      if(new_hash[lang] == nil)
+        new_hash[lang] = data
+      end
+
+      if new_hash[lang][:style] == nil
+        new_hash[lang][:style] = [style]
+      else
+        new_hash[lang][:style].push(style)
+      end
+    end
+  end
+  return new_hash
 end
